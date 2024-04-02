ece7c868b461f689bcb1bf2503ee9aea2c3c5e3a
Quas <ctsn99@gmail.com>
2024-04-02 14:35:14 -0400

    Initial bot load

--- src/main/java/com/quas/ygo_rdl_bot/RushDuelLinksBot.java
+++ src/main/java/com/quas/ygo_rdl_bot/RushDuelLinksBot.java
@@ -1,8 +1,5 @@
 package com.quas.ygo_rdl_bot;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -10,12 +7,6 @@ import net.dv8tion.jda.api.entities.Activity;
 public class RushDuelLinksBot {
 	
 	public static void main(String[] args) throws InterruptedException {
-		
-		try (PrintWriter out = new PrintWriter("properties.json")) {
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		
 		JDA bot = JDABuilder.createDefault(BotProperties.getToken()).build();
 		bot.awaitReady();
