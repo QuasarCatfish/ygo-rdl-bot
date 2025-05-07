0913c78da10758703911d3a924c3b662cac04ac0
Quas <ctsn99@gmail.com>
2025-05-07 04:53:10 -0400

    Add new card styles

--- src/main/java/com/quas/ygo_rdl_bot/data/CardStyle.java
+++ src/main/java/com/quas/ygo_rdl_bot/data/CardStyle.java
@@ -1,7 +1,11 @@
 package com.quas.ygo_rdl_bot.data;
 
 public enum CardStyle {
-	ORIGINAL("Original"), ALT_ART("Alternate Art"), OR_STYLE("OR Style");
+	ORIGINAL("Original"),
+	ALT_ART("Alternate Art"),
+	ALT_ART_2("2nd Alternate Art"),
+	OR_STYLE("OR Style"),
+	OR_STYLE_ALT_ART("OR Style Alternate Art");
 
 	private String cardStyleName;
 	private CardStyle(String cardStyleName) {
